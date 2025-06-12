20cd64329c952dd3a6fac569c3b44695b1004c0d
Stelios Papargyris <papargirisstelios3@gmail.com>
2025-06-12 13:32:05 +0300

    10chips_no_noise_minmax_every_row

--- utils/config.py
+++ utils/config.py
@@ -11,14 +11,14 @@ early_stopping_max_number = 20
 learning_rate = 1e-3
 num_epochs = 500
 num_classes = 4
-chip_exclude = 100
+chip_exclude = 10
 num_chips = list(range(1, chip_exclude))
 baseline_chip = 4
 chip_column = "Chip"
 class_column = "Class"
 target_class = 4
-num_chip_selection = 100
-base_path = f"/Users/steliospapargyris/Documents/MyProjects/data_thesis/every_sample_minmax_every_row/fts_mzi_dataset/{num_chip_selection}chips"
+num_chip_selection = 10
+base_path = f"/Users/steliospapargyris/Documents/MyProjects/data_thesis/every_sample_minmax_every_row/fts_mzi_dataset/{num_chip_selection}chips_no_noise"
 current_path = f"{base_path}"
 test_file_path = f'{base_path}/{chip_exclude}.csv'
 matplotlib.use('Agg')  # Use a non-interactive backend
